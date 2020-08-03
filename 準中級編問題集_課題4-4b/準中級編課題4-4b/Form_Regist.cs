9d0bb1bd19ee355ed2f10649198267116ffce117
yasuyu55 <yu.yasu55@gmail.com>
2020-08-03 23:00:56 +0900

    Revert "不必要なファイルを削除"
    This reverts commit ae4c155ffe2b474d283632a42b891e445c491f2a.

---
+++ 準中級編問題集_課題4-4b/準中級編課題4-4b/Form_Regist.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace 準中級編課題4_4b
+{
+    /// <summary>
+    /// 生徒の名前と国語・英語・数学の成績を登録する
+    /// </summary>
+    public partial class Form_Regist : Form
+    {
+        public Form_Regist()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Scoresクラスのオブジェクトscoresを生成
+        /// </summary>
+        private Scores scores = new Scores();
+
+        /// <summary>
+        /// ScoresクラスのオブジェクトScoreRegistを生成
+        /// </summary>
+        public Scores ScoreRegist
+        {
+            set
+            {
+                this.scores = value;
+            }
+            get
+            {
+                return this.scores;
+            }
+        }
+
+        /// <summary>
+        /// string[]型のListを宣言
+        /// </summary>
+        private List<string[]> list = new List<string[]>();
+
+        /// <summary>
+        /// フォームを起動したら、STUDENTテーブルから名前情報をcomBboxに格納する
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form_Regist_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // DELETEDが0のときのSTUDENTテーブルのデータを表示
+                string sqlCommand = @"SELECT * FROM STUDENT_TABLE WHERE DELETED = 0;";
+
+                string[] colums = {
+                                    "STUDENT_ID",
+                                    "STUDENT_NAME",
+                                    "DELETED"
+                                   };
+
+                // sqlコマンドを実行し、カラム名をListに保存する
+                listFromTableData(sqlCommand, colums);
+
+                // Listデータから名前をコンボボックスに追加
+                addComboBoxFromList();
+
+                // 0で初期化
+                this.comboBox_Name.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// sqlコマンドを実行し、カラム名をListに保存する
+        /// </summary>
+        /// <param name="sqlCommand">SQL文</param>
+        /// <param name="colums">カラム名</param>
+        private void listFromTableData(string sqlCommand, string[] colums)
+        {
+            string dbFile = Application.StartupPath + @"\results2.db";
+            SQLiteConnection con = null;
+            SQLiteCommand com = null;
+            SQLiteDataReader reader = null;
+
+            try
+            {
+                // 接続文字列
+                string connectionString = String.Format("Data Source={0};Version=3;", dbFile);
+                // DB Open
+                con = new SQLiteConnection(connectionString);
+                con.Open();
+                com = con.CreateCommand();
+
+                // テーブルのデータを全て表示する
+                com.CommandText = sqlCommand;
+                reader = com.ExecuteReader();
+
+                this.list.Clear();
+
+                while (reader.Read())
+                {
+                    string[] s = new string[colums.Length];
+
+                    for (int i = 0; i < colums.Length; i++)
+                    {
+                        s[i] = reader[colums[i]].ToString();
+                    }
+
+                    this.list.Add(s);
+                }
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+                if (com != null)
+                {
+                    com.Dispose();
+                }
+                if (con != null)
+                {
+                    con.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// コンボボックスに名前を追加
+        /// </summary>
+        private void addComboBoxFromList()
+        {
+            this.comboBox_Name.Items.Clear();
+
+            for (int i = 0; i < this.list.Count; i++)
+            {
+                string name = this.list[i][1];
+                this.comboBox_Name.Items.Add(name);
+            }
+        }
+
+        /// <summary>
+        /// ボタンを押したら、Registフォームに入力されたデータを
+        /// メインフォームのリストビューの最後の行に追加する
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_OK_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+
+            this.scores.StudentID = this.list[this.comboBox_Name.SelectedIndex][0];
+
+            this.scores.Name = this.comboBox_Name.SelectedItem.ToString();
+            this.scores.Japanese = this.numericUpDown_Japanese.Value.ToString();
+            this.scores.Math = this.numericUpDown_Math.Value.ToString();
+            this.scores.English = this.numericUpDown_English.Value.ToString();
+        }
+    }
+}
---
+++ 準中級編問題集_課題4-4b/準中級編課題4-4b/Form_Update.designer.cs
@@ -0,0 +1,171 @@
+﻿namespace 準中級編課題4_4b
+{
+    partial class Form_Update
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown_English = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown_Math = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown_Japanese = new System.Windows.Forms.NumericUpDown();
+            this.label_English = new System.Windows.Forms.Label();
+            this.label_Math = new System.Windows.Forms.Label();
+            this.label_Japanese = new System.Windows.Forms.Label();
+            this.label_Name = new System.Windows.Forms.Label();
+            this.textBox_Name = new System.Windows.Forms.TextBox();
+            this.button_Cancel = new System.Windows.Forms.Button();
+            this.button_OK = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_English)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Math)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Japanese)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown_English
+            // 
+            this.numericUpDown_English.Location = new System.Drawing.Point(129, 155);
+            this.numericUpDown_English.Name = "numericUpDown_English";
+            this.numericUpDown_English.Size = new System.Drawing.Size(120, 19);
+            this.numericUpDown_English.TabIndex = 21;
+            // 
+            // numericUpDown_Math
+            // 
+            this.numericUpDown_Math.Location = new System.Drawing.Point(129, 118);
+            this.numericUpDown_Math.Name = "numericUpDown_Math";
+            this.numericUpDown_Math.Size = new System.Drawing.Size(120, 19);
+            this.numericUpDown_Math.TabIndex = 20;
+            // 
+            // numericUpDown_Japanese
+            // 
+            this.numericUpDown_Japanese.Location = new System.Drawing.Point(129, 88);
+            this.numericUpDown_Japanese.Name = "numericUpDown_Japanese";
+            this.numericUpDown_Japanese.Size = new System.Drawing.Size(120, 19);
+            this.numericUpDown_Japanese.TabIndex = 19;
+            // 
+            // label_English
+            // 
+            this.label_English.AutoSize = true;
+            this.label_English.Location = new System.Drawing.Point(36, 157);
+            this.label_English.Name = "label_English";
+            this.label_English.Size = new System.Drawing.Size(29, 12);
+            this.label_English.TabIndex = 18;
+            this.label_English.Text = "英語";
+            // 
+            // label_Math
+            // 
+            this.label_Math.AutoSize = true;
+            this.label_Math.Location = new System.Drawing.Point(36, 125);
+            this.label_Math.Name = "label_Math";
+            this.label_Math.Size = new System.Drawing.Size(29, 12);
+            this.label_Math.TabIndex = 17;
+            this.label_Math.Text = "数学";
+            // 
+            // label_Japanese
+            // 
+            this.label_Japanese.AutoSize = true;
+            this.label_Japanese.Location = new System.Drawing.Point(36, 90);
+            this.label_Japanese.Name = "label_Japanese";
+            this.label_Japanese.Size = new System.Drawing.Size(29, 12);
+            this.label_Japanese.TabIndex = 16;
+            this.label_Japanese.Text = "国語";
+            // 
+            // label_Name
+            // 
+            this.label_Name.AutoSize = true;
+            this.label_Name.Location = new System.Drawing.Point(36, 48);
+            this.label_Name.Name = "label_Name";
+            this.label_Name.Size = new System.Drawing.Size(29, 12);
+            this.label_Name.TabIndex = 15;
+            this.label_Name.Text = "名前";
+            // 
+            // textBox_Name
+            // 
+            this.textBox_Name.Location = new System.Drawing.Point(140, 45);
+            this.textBox_Name.Name = "textBox_Name";
+            this.textBox_Name.ReadOnly = true;
+            this.textBox_Name.Size = new System.Drawing.Size(100, 19);
+            this.textBox_Name.TabIndex = 14;
+            // 
+            // button_Cancel
+            // 
+            this.button_Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button_Cancel.Location = new System.Drawing.Point(149, 198);
+            this.button_Cancel.Name = "button_Cancel";
+            this.button_Cancel.Size = new System.Drawing.Size(75, 23);
+            this.button_Cancel.TabIndex = 13;
+            this.button_Cancel.Text = "Cancel";
+            this.button_Cancel.UseVisualStyleBackColor = true;
+            // 
+            // button_OK
+            // 
+            this.button_OK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button_OK.Location = new System.Drawing.Point(38, 198);
+            this.button_OK.Name = "button_OK";
+            this.button_OK.Size = new System.Drawing.Size(75, 23);
+            this.button_OK.TabIndex = 12;
+            this.button_OK.Text = "OK";
+            this.button_OK.UseVisualStyleBackColor = true;
+            this.button_OK.Click += new System.EventHandler(this.button_OK_Click);
+            // 
+            // Form_Update
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.numericUpDown_English);
+            this.Controls.Add(this.numericUpDown_Math);
+            this.Controls.Add(this.numericUpDown_Japanese);
+            this.Controls.Add(this.label_English);
+            this.Controls.Add(this.label_Math);
+            this.Controls.Add(this.label_Japanese);
+            this.Controls.Add(this.label_Name);
+            this.Controls.Add(this.textBox_Name);
+            this.Controls.Add(this.button_Cancel);
+            this.Controls.Add(this.button_OK);
+            this.Name = "Form_Update";
+            this.Text = "更新";
+            this.Load += new System.EventHandler(this.Form_Update_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_English)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Math)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Japanese)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown_English;
+        private System.Windows.Forms.NumericUpDown numericUpDown_Math;
+        private System.Windows.Forms.NumericUpDown numericUpDown_Japanese;
+        private System.Windows.Forms.Label label_English;
+        private System.Windows.Forms.Label label_Math;
+        private System.Windows.Forms.Label label_Japanese;
+        private System.Windows.Forms.Label label_Name;
+        private System.Windows.Forms.TextBox textBox_Name;
+        private System.Windows.Forms.Button button_Cancel;
+        private System.Windows.Forms.Button button_OK;
+    }
+}
